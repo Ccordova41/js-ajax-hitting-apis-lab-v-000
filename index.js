18fa41ad9f22614bfa64f524cfb0132deba4445c
Catherine Cordova <ccordova41@gmail.com>
2017-11-21 23:57:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,4 +28,19 @@ function displayRepositories() {
   document.getElementById("repositories").innerHTML = repoList
 }
 
-  
+function getCommits(el) {
+  const repoName = el.dataset.repository
+  const uri = rootURL + "/repos/" + el.dataset.username + "/" + repoName + "/commits"
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayCommits)
+  req.open("GET", uri)
+  req.send()
+}
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = commitsList
+}
+
+
