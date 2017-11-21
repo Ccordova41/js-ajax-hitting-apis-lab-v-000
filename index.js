ee9522c74cff125a01b5af8d1d413243d4b07e30
Catherine Cordova <ccordova41@gmail.com>
2017-11-21 23:46:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,23 +37,3 @@ function displayRepositories() {
     req.send()
   }
 
-  function displayCommits() {
-    const commits = JSON.parse(this.responseText)
-    const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
-    document.getElementById("details").innerHTML = commitsList
-  }
-
-  function getBranches(el) {
-    const repoName = el.dataset.repository
-    const uri = rootURL + "/repos/" + el.dataset.username + "/" + repoName + "/branches"
-    const req = new XMLHttpRequest()
-    req.addEventListener("load", displayBranches)
-    req.open("GET", uri)
-    req.send()
-  }
-
-  function displayBranches() {
-    const branches = JSON.parse(this.responseText)
-    const branchesList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>').join('')}</ul>`
-    document.getElementById("details").innerHTML = branchesList
-  }
